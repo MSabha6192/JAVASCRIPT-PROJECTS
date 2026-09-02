c2e2bfb2273ef8761ff4b84af01cc446b47ee809
MSabha6192 <mohammed.6192@hotmail.com>
2026-09-02 00:02:18 -0400

    Completed 1st JavaScript Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var food = "Dessert",
+    country = "USA",
+    language = "English"; //Created variable for food, country, and language
+document.write(country); //Displays USA as text
+window.alert("Welcome to my first submission!"); //Text will appear on the popup window 
+Sent1 = "This is the beginning of the string";
+Sent2 = " and this is the end of the string"; //Assigned variables
+document.write("<br>"); //To move to the next line
+document.write(Sent1 + Sent2); //Added an addition operator  the 2 strings
+17 + 23 //expression
+document.write("<br>");
+document.write(17 + 23); //Produces the result of the expression
